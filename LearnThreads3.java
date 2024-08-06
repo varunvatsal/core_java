3205f47acfa9fbc3be1974b592d064244b9b4f7d
varun vatsal <varunvatsal963@gmail.com>
2024-08-06 22:18:58 +0530

    basic changes

--- LearnThreads3.java
+++ LearnThreads3.java
@@ -22,11 +22,18 @@ public class LearnThreads3 {
         Thread t1 = new Thread(objA);
         Thread t2 = new Thread(objB);
 
+        t1.setName("Thread name: Hi");
+        t2.setName("Tread name: Hello");
+        System.out.println(t1.getName());;
+        System.out.println(t2.getName());;
         t1.start();
         t2.start();
 
         t1.join();
         t2.join();
+
+        System.out.println(t1.isAlive());//return true if thread is still running
+        System.out.println(t2.isAlive());//false otherwi
        
         System.out.println(c.cnt);
 
