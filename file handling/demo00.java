a3d3141aeb899eb2eb806630a722bce05d577570
varun vatsal <varunvatsal963@gmail.com>
2024-08-13 15:30:16 +0530

    basic changes

--- file handling/demo00.java
+++ file handling/demo00.java
@@ -5,11 +5,13 @@ import java.io.IOException;
 public class demo00 {
     public static void main(String[] args) {
         try(FileReader fr = new FileReader("Risk of Developing or Dying From Cancer.csv")) {
+            // FileReader fr = new FileReader("Risk of Developing or Dying From Cancer.csv");
             int letter = fr.read();
             while(fr.ready()){
                 System.out.print((char) letter);
                 letter = fr.read();
             }
+            // fr.close();
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
